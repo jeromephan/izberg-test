b2c36f40ed1ef6d5977c4e29f062cf87bfe768ae
Jerome Phan <jerome@bluecoders.io>
2020-09-14 02:29:38 +0200

    Add logo

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Topbar.scss';
+import logo from '../../assets/img/logo.png';
 
 const Topbar = () => {
   return (
     <div className='topbar'>
-      <NavLink className='topbar__link' to='/'>
-        Pokemon List
-      </NavLink>
-      <NavLink className='topbar__link' to='/favorites'>
-        Favorites
-      </NavLink>
+      <img className='topbar__logo' src={logo} alt='logo' />
+      <div className='topbar__wrapper'>
+        <NavLink className='topbar__link' to='/'>
+          Pokemon List
+        </NavLink>
+        <NavLink className='topbar__link' to='/favorites'>
+          Favorites
+        </NavLink>
+      </div>
     </div>
   );
 };
